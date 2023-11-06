1f5b900fdced39143379fbdc747ac00243a8f0a9
AnujSinghR <112078479+AnujSinghR@users.noreply.github.com>
2023-11-06 11:56:04 +0530

    Update create-table.js
    remove the garbage

--- create-table.js
+++ create-table.js
@@ -20,12 +20,3 @@ let a;
       console.log(result[0])
     });
   });
-//  con.connect(function(err) {
-//  if (err) throw err;
-//  console.log("Connected!");
-//  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-//  con.query(sql, function (err, result) {
-//    if (err) throw err;
-//    console.log("1 record inserted");
-//  });
-//});
